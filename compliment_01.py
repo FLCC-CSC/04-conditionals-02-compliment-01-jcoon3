c116f1b2401abb99e7b97079fa5f88537b97205d
jcoon3 <jcoon3@fingerlakes.edu>
2025-09-30 18:46:59 +0000

    compliment.py take 1

--- compliment_01.py
+++ compliment_01.py
@@ -1,81 +1,15 @@
-# FILE NAME - compliment_01.py
-
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+# Name: Justin Coon
+# Date: 09-29-2025
+# Description A program that asks a user if the want a compliment.
 
 
+# Variable declaration for user input
+compliment = input("Would you like a compliment: ")
 
-# 1. Make sure you fill out the comments above
-# 2. Write your code in the proper spot
-# 3. Be sure to answer the Reflection Questions and Attestation below
-# 4. The Sample Output has been included in this code for your convenience
+# An if statement checking that the conition of the input is met and printscompliment
+if compliment == 'yes':
+    print("You have wonderful eyes.")
 
-
-
-########## ENTER YER CODE BELOW THIS LINE ##########
-
-
-
-
-
-
-
-
-
-
-########### END YER CODE ABOVE THIS LINE ###########
-
+# Thank you for playing statement    
+print("Thank you for playing.")
     
-
-
-
-########################################
-#          SAMPLE OUTPUT
-########################################
-
-'''
-Would you like a compliment? yes
-You have wonderful eyes.
-Thank you for playing.
-'''
-
-
-'''
-Would you like a compliment? y
-Thank you for playing.
-'''
-
-
-'''
-Would you like a compliment? Yes
-Thank you for playing.
-'''
-
-
-'''
-Would you like a compliment? no
-Thank you for playing.
-'''
-
-
-########################################
-#          REFLECTION QUESTIONS
-########################################
-
-'''
-
-1. On a scale of 1 to 10 (where 10 is the hardest), how would you rate this lab?
-
-
-
-
-2. What was the hardest part of this lab?
-
-
-
-
-
-
-
-'''
